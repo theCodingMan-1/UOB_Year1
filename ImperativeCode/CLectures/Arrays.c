2aafc3cac934c2e64525c6a115919c971c715b77
theCodingMan-1 <brynmorpeters@gmail.com>
2024-09-27 09:52:25 +0100

    C 27.09.24

---
+++ ImperativeCode/CLectures/Arrays.c
@@ -0,0 +1,14 @@
+
+
+int array(void){
+    int seq[3]; // declare an erray of 3 int variables
+    seq[0] = 10; // initialises first variable
+    seq[1] = 5; // initialises second variable
+    seq[2] = 4; // initialises third (final) variable
+
+    return 0;
+}
+
+int array2(void) {
+    int seq[] = {10, 5, 4}; // legal alternative for seq[3]
+}
