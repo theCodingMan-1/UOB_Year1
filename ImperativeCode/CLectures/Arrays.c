e4f0e5519e668d2eaf6376e640044de37e16cd6e
theCodingMan-1 <brynmorpeters@gmail.com>
2024-10-03 12:51:52 +0100

    CLecture 03.10.2024

--- ImperativeCode/CLectures/Arrays.c
+++ ImperativeCode/CLectures/Arrays.c
@@ -6,9 +6,15 @@ int array(void){
     seq[1] = 5; // initialises second variable
     seq[2] = 4; // initialises third (final) variable
 
+    // seq[3] means it takes up 3 spaces in memory. 
+    // Trying to find seq[800] won't work because it is outside the memory assigned to the array
+
     return 0;
 }
 
 int array2(void) {
     int seq[] = {10, 5, 4}; // legal alternative for seq[3]
+    return 0;
 }
+
+// arrays can't be returned from functions directly. 
--- ImperativeCode/CLectures/Characters.c
+++ ImperativeCode/CLectures/Characters.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int character(void) {
     char c = 65;
@@ -16,6 +17,20 @@ int string(void) {
     // and 0 is the decimal ASCII for the null character
     // string needs to be terminated by null at the end
 
-    char textB[8] = " there!" // automatically splits it up
-    printf("%s%s\n", textA, textB) // prints "Hi there!"
+    char textB[8] = " there!"; // automatically splits it up
+    printf("%s%s\n", textA, textB); // prints "Hi there!"
+
+    char text[] = {'H', 'i', '\0'};
+    // '\0' represents the null character and 
+    // basically tells the program to stop when going through the list
+    
 }
+
+// char is just a small integer where for instance 'A' is a synonym for the number 65,
+// since 65 is the ASCII value of the character 'A'.
+// Note '0' is a synonym for 48 not 0
+
+// To define characters as ASCII symbols we use a backslash \ to define them. 
+// '\0' is not 48 but the NULL character, which has the ASCII value of 0.
+
+
