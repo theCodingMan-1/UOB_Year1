9b3241093a4cf9cfce65339a0d656efe7763cf48
theCodingMan-1 <brynmorpeters@gmail.com>
2024-10-03 16:54:13 +0100

    HSLecture 03.10.2024

--- FunctionalCode/HSLectures/PatternsLists.hs
+++ FunctionalCode/HSLectures/PatternsLists.hs
@@ -1,4 +1,21 @@
 
+func :: [Int] -> Int
+func [] = 0
+-- if the input matches the pattern func [], the empty list, 0 is returned
+
+func (x : xs) = 1 + func xs 
+-- if the input matches the pattern func [xs] where xs stands for a list, it will recursively call itself
+  -- adding one each time. This calculates the length of the list.
+  -- Here x is the first element and xs is the rest of the list.
+  
+-- Example: func ['a', 'b', 'c'] 
+  -- = 1 + func ['b', 'c']
+  -- = 1 + (1 + func ['c'])
+  -- = 1 + (1 + (1 + func []))
+  -- = 1 + (1 + (1 + 0))
+  -- = 1 + (1 + (1))
+  -- = 1 + (2)
+  -- = 3
 
 
 fac :: Int  -> Int
@@ -24,6 +41,7 @@ sum' [] = 0 -- in case of the empty list, it returns 0 (base case)
 sum' (x : []) = x -- in case of one element, it returns the single element
 sum' (x : xs) = x + sum' xs  -- in case of more than one element, it returns the first element, plus sum' of the rest of the list
 -- xs stands for the rest of the list. 
+
 
 
 
