1b8e61bd0193f2f8f1ac038f3fa7021ebc2b0cc7
theCodingMan-1 <brynmorpeters@gmail.com>
2024-10-28 22:38:15 +0000

    ggggggggnilm7

--- ImperativeCode/CCourseWork/ListChallange/visualise.c
+++ ImperativeCode/CCourseWork/ListChallange/visualise.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
@@ -35,19 +36,93 @@ int convert(const char length[]) {
 }
 
 
+char* character(char number[]) {
+    // int num = convert(number);
+    // printf("%d\n", num);
+    if (number[0] == '0') return "Input Error"; // no leading 0 -> "03" rejected
+
+    char *endptr;
+    int num = strtol(number, &endptr, 10);
+
+
+    if (endptr == number) return "Input Error";
+    // this means no digits were found within the string (i.e "hello")
+
+    else if (*endptr != '\0') return "Input Error";
+    // invalid character within the set (i.e "4y")
+    // this is because the characters do not have null characters after them
+
+    else if ((num < 0) || (num > 255)) return "Input Error";
+
+    else {
+        int power = 7;
+        int q = 0;
+        int r = num;
+        char output[11];
+        while (power > 0) {
+            int divider = pow(2, power);
+            q = r / divider;
+            r = r % divider;
+            char qStr[10];
+            sprintf(qStr, "%d", q);
+            strcat(output, qStr);
+            printf("%s\n", qStr);
+            printf("%s\n\n", output);
+            if (power == 4) strcat(output, " ");
+            power--;
+        }
+        char rStr[11];
+        sprintf(rStr, "%d", r);
+        strcat(output, rStr);
+        strcat(output, "\0");
+        printf("%s\n", rStr);
+        printf("%s\n\n", output);
+
+        return output;
+    }
+}
+
+void assert(int line, bool b) {
+    if (b) return;
+    printf("The test on line %d fails.\n", line);
+    exit(1);
+}
+
+
+
+void testCharacter(void){
+    assert(__LINE__, character("0") == "0000 0000");
+    assert(__LINE__, character("43") == "Input Error");
+    assert(__LINE__, character("255") == "1111 1111");
+    assert(__LINE__, character("-1") == "Input Error");
+    assert(__LINE__, character("256") == "Input Error");
+    assert(__LINE__, character("4.5") == "Input Error");
+    assert(__LINE__, character("-0.65") == "Input Error");
+    assert(__LINE__, character("-1") == "Input Error");
+
+
+}
+
+
+
+void test(void) {
+    testCharacter();
+    printf("All Tests Pass!\n");
+}
+
 
 
 int main(int n, char *args[n]) {
     setbuf(stdout, NULL);
-    // if (n == 1) {
-    //     test();
-    // }
-    if (n == 3) {
+    if (n == 1) {
+        test();
+    }
+    else if (n == 3) {
         if (strcmp(args[1], "char") == 0){
-            printf("this is a char");
+            // int num = character(args[2]);
+            printf("%s\n", character(args[2]));
         }
-        int num = convert(args[2]);
-        printf("Type of num is %d\n", num);
+
         // if (a == char) printf("hey\n");
         // else printf("no\n");
         
