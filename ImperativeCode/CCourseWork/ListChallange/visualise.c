cc4c239726d1f1f0736e0474b4046c3450f11385
theCodingMan-1 <brynmorpeters@gmail.com>
2024-10-29 20:24:13 +0000

    AWEFrehtjrykuliulit6u5y4trew

--- ImperativeCode/CCourseWork/ListChallange/visualise.c
+++ ImperativeCode/CCourseWork/ListChallange/visualise.c
@@ -6,11 +6,11 @@
 #include <stdbool.h>
 #include <limits.h>
 
-long convertToNum(char number[]) {
+long long convertToNum(char number[]) {
     if (number[0] == '0' && number[1] != '\0') return -1; // no leading 0 -> "03" rejected
 
     char *endptr;
-    long num = strtol(number, &endptr, 10);
+    long long num = strtol(number, &endptr, 10);
 
 
     if (endptr == number) return -1;
@@ -23,23 +23,33 @@ long convertToNum(char number[]) {
     else return num;
 }
 
-char* convertToBin(long num, int power) {
+
+
+
+char* convertToBin(long long num, int power) {
     const int constantPower = power;
     int adder = 0;
     int q = 0;
-    long r = abs(num);
+    if (num / 2 == -4611686018427387904) {
+        num++;
+    }
+    long long r = labs(num);
     int memory = constantPower + (constantPower / 4) + 2;
     char output[memory];
+    char *p = output;
     while (power > 0) {
-        long divider = pow(2, power);
-        q = r / divider;
-        r = r % divider;
+        if (power >= 63) q = 0;
+        else {
+            long long divider = pow(2, power);
+            q = r / divider;
+            r = r % divider;
+        }
+
         char qStr[2];
             
         sprintf(qStr, "%d", q);
         output[constantPower - power + adder] = qStr[0];
-        
-            
+                
         if (power % 4 == 0) {
             adder++;
             output[constantPower - power + adder] = ' ';
@@ -48,7 +58,7 @@ char* convertToBin(long num, int power) {
         power--;
     }
     char rStr[2];
-    sprintf(rStr, "%d", r);
+    sprintf(rStr, "%lli", r);
     output[memory - 2] = rStr[0];
 
     if (num < 0) {
@@ -65,16 +75,14 @@ char* convertToBin(long num, int power) {
         }    
     }
     output[memory - 1] = '\0';
-    return output;
+    return p;
 
 }
 
 
 
-
-
 char* character(char number[]) {
-    long num;
+    long long num;
     if ((number[0] == '-') && (number[1] == '1') && (number[2] == '\0')) {
         num = -1;
     }
@@ -92,7 +100,7 @@ char* character(char number[]) {
 
 
 char* integer(char number[]) {
-    long num;
+    long long num;
     if ((number[0] == '-') && (number[1] == '1') && (number[2] == '\0')) {
         num = -1;
     }
@@ -108,13 +116,35 @@ char* integer(char number[]) {
     return convertToBin(num, power);
 }
 
+char* makeLong(char number[]) {
+    long long num;
+    if ((number[0] == '-') && (number[1] == '1') && (number[2] == '\0')) {
+        num = -1;
+    }
+    else {
+        num = convertToNum(number);
+        long long newNum = num;
+        if (num < 0) newNum++;
+        printf("%lli\n", newNum);
+        if (num == -1) return "Input Error";
+
+        
+
+        else if ((newNum < -9223372036854775807) || (newNum > 9223372036854775807)) return "Input Error";
+    }
+
+    int power = 63;  
+    
+    return convertToBin(num, power);
+
+}
 
 
 
 
 char* unCharacter(char number[]) {
 
-    long num = convertToNum(number);
+    long long num = convertToNum(number);
     if (num == -1) return "Input Error";
 
     else if ((num < 0) || (num > 255)) return "Input Error";
@@ -123,6 +153,23 @@ char* unCharacter(char number[]) {
     int power = 7;
 
     return convertToBin(num, power);
+}
+
+
+
+char* unInteger(char number[]) {
+
+
+    long long num = convertToNum(number);
+    if (num == -1) return "Input Error";
+
+    else if ((num < 0) || (num > 4294967295)) return "Input Error";
+
+    
+    int power = 31;
+
+    return convertToBin(num, power);
+
 }
 
 
@@ -157,19 +204,19 @@ void testCharacter(void) {
 
 
 void testInteger(void) {
-    assert(__LINE__, strcmp(integer("-128"), "1000 0000") == 0);
+    assert(__LINE__, strcmp(integer("-2147483648"), "1000 0000 0000 0000 0000 0000 0000 0000") == 0);
     assert(__LINE__, strcmp(integer("-987654321"), "1100 0101 0010 0001 1001 0111 0100 1111") == 0);
     assert(__LINE__, strcmp(integer("1234567890"), "0100 1001 1001 0110 0000 0010 1101 0010") == 0);
-    assert(__LINE__, strcmp(integer("127"), "0111 1111") == 0);
-    assert(__LINE__, strcmp(integer("-1"), "1111 1111") == 0);
-    assert(__LINE__, strcmp(integer("-129"), "Input Error") == 0);
-    assert(__LINE__, integer("128") == "Input Error");
-    assert(__LINE__, integer("4.5") == "Input Error");
-    assert(__LINE__, integer("-0.65") == "Input Error");
-    assert(__LINE__, integer("05") == "Input Error");
-    assert(__LINE__, integer("4z") == "Input Error");
-    assert(__LINE__, integer("4T3") == "Input Error");
-    assert(__LINE__, integer("hello") == "Input Error");
+    assert(__LINE__, strcmp(integer("2147483647"), "0111 1111 1111 1111 1111 1111 1111 1111") == 0);
+    assert(__LINE__, strcmp(integer("-1"), "1111 1111 1111 1111 1111 1111 1111 1111") == 0);
+    assert(__LINE__, strcmp(integer("-2147483649"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("2147483648"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("4.5"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("-0.65"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("05"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("4z"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("4T3"), "Input Error") == 0);
+    assert(__LINE__, strcmp(integer("hello"), "Input Error") == 0);
 
 }
 
@@ -193,9 +240,28 @@ void testUnCharacter(void){
 
 
 
+void testUnInteger(void) {
+    assert(__LINE__, strcmp(unInteger("0"), "0000 0000 0000 0000 0000 0000 0000 0000") == 0);
+    assert(__LINE__, strcmp(unInteger("1234567890"), "0100 1001 1001 0110 0000 0010 1101 0010") == 0);
+    assert(__LINE__, strcmp(unInteger("4294967295"), "1111 1111 1111 1111 1111 1111 1111 1111") == 0);
+    assert(__LINE__, strcmp(unInteger("-1"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("4294967296"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("4.5"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("-0.65"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("05"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("4z"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("4T3"), "Input Error") == 0);
+    assert(__LINE__, strcmp(unInteger("hello"), "Input Error") == 0);
+
+}
+
+
+
 void test(void) {
     testCharacter();
+    testInteger();
     testUnCharacter();
+    testUnInteger();
     printf("All Tests Pass!\n");
 }
 
@@ -209,10 +275,14 @@ int main(int n, char *args[n]) {
     else if (n == 3) {
         if (strcmp(args[1], "char") == 0) printf("%s\n", character(args[2]));
         else if (strcmp(args[1], "int") == 0) printf("%s\n", integer(args[2]));
+        else if (strcmp(args[1], "long") == 0) printf("%s\n", makeLong(args[2]));
     }
     else if (n == 4) {
         if ((strcmp(args[1], "unsigned") == 0) && (strcmp(args[2], "char") == 0)){
             printf("%s\n", unCharacter(args[3]));
+        }
+        else if ((strcmp(args[1], "unsigned") == 0) && (strcmp(args[2], "int") == 0)){
+            printf("%s\n", unInteger(args[3]));
         }
         
 
