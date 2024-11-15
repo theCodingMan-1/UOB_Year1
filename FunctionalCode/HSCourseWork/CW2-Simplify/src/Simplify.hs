d527e41574e6df466c10bcb8daf4706e0a8a7cb4
theCodingMan-1 <brynmorpeters@gmail.com>
2024-11-15 20:49:24 +0000

    a

--- FunctionalCode/HSCourseWork/CW2-Simplify/src/Simplify.hs
+++ FunctionalCode/HSCourseWork/CW2-Simplify/src/Simplify.hs
@@ -4,7 +4,8 @@ module Simplify where
 
 import Expr
 import Poly
-import Control.Concurrent (yield)
+import Control.Concurrent (yield, waitQSem)
+import Data.Bits (Bits(xor))
 
 --------------------------------------------------------------------------------
 -- * Task 1
@@ -31,10 +32,6 @@ add (ExpX x) (ExpX y)
     | otherwise = Op AddOp (ExpX x) (ExpX y)
 
 
--- add (Op AddOp x y) z = Op AddOp (add x y) z
--- add x (Op AddOp y z) = Op AddOp x (add y z)
--- add (Op AddOp x y) (Op AddOp z a) = Op AddOp (add x y) (add z a)
-
 
 add x y
     | x == NumLit 0 = y
@@ -42,38 +39,6 @@ add x y
 
     |otherwise = Op AddOp x y
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- add :: Expr -> Expr -> String
--- add (NumLit x) (ExpX y)
---     | y == 0 = prettyExpr (NumLit (x + 1))
---     | x == 0 = prettyExpr (ExpX y)
---     | otherwise = prettyExpr (Op AddOp (NumLit x) (ExpX y))
--- add (ExpX x) (NumLit y)
---     | x == 0 = prettyExpr (NumLit (y + 1))
---     | y == 0 = prettyExpr (ExpX x)
---     | otherwise = prettyExpr (Op AddOp (ExpX x) (NumLit y))
--- add (NumLit x) (NumLit y) = prettyExpr (NumLit (x + y))
--- add (ExpX x) (ExpX y) 
---     | x == 0 && y == 0 = prettyExpr (NumLit 2)
---     | x == 0 = prettyExpr (Op AddOp (NumLit 1) (ExpX y))
---     | y == 0 = prettyExpr (Op AddOp (ExpX x) (NumLit 1))
---     | x == y = prettyExpr (Op MulOp (NumLit 2) (ExpX x))
---     | otherwise = prettyExpr (Op AddOp (ExpX x) (ExpX y))
-
--- add x y = prettyExpr(Op AddOp x y)
 
 --------------------------------------------------------------------------------
 -- * Task 2
@@ -125,8 +90,50 @@ mulAll xs = foldr mul (NumLit 1) xs
 -- * Task 5
 -- Define exprToPoly, which converts an expression into a polynomial.
 
+maximum' :: Ord a => [a] -> a
+maximum' [x] = x
+maximum' (x : x' : xs)
+    | x >= x' = maximum' (x : xs)
+    | otherwise = maximum' (x' : xs)
+
+
+
+
+makePowerList :: Expr -> [Expr]
+makePowerList (ExpX x) = [ExpX x]
+makePowerList (NumLit x) = [NumLit x]
+makePowerList (Op AddOp x y) = makePowerList x ++ makePowerList y
+
+count :: Expr -> [Expr] -> Int
+count a [] = 0
+count a (x : xs)
+    | a == x = 1 + count a xs
+    | otherwise = count a xs
+
+makeCoList :: [Expr] -> [Int]
+makeCoList (x : xs) = count x (x : xs) : makeCoList (filter (/= x) xs)
+-- makeCoList [] = []
+-- makeCoList xs = count (maximum' xs) xs : makeCoList (filter (/= maximum' xs) xs)
+
+makeList :: Int -> [Int] -> [Int]
+makeList a xs = makeList' [a] xs
+    where
+        makeList' :: [Int] -> [Int] -> [Int]
+        makeList' list [] = list
+        makeList' list (x : xs) = makeList' (list ++ [x]) xs
+
+--ExpX 4
+-- >>> [1, 0, 0, 0, 0]
+-- >>> x^4
+
 exprToPoly :: Expr -> Poly
-exprToPoly expr = error "Implement me!"
+exprToPoly (NumLit x) = listToPoly [x]
+exprToPoly (ExpX x) = listToPoly (makeList 1 [1..x])
+exprToPoly (Op AddOp x y) = addPoly (exprToPoly x) (exprToPoly y)
+exprToPoly (Op MulOp x y) = mulPoly (exprToPoly x) (exprToPoly y)
+
+-- exprToPoly x = listToPoly (makeCoList (makePowerList x))
+-- exprToPoly (Op AddOp x y) = exprToPoly ([x] + [y])
 
 --------------------------------------------------------------------------------
 -- * Task 6
---
+++ ImperativeCode/CLectures/Macros.c
@@ -0,0 +1,2 @@
+#define PI 3.14159
+#define DISC(radius) ((PI) * (radius) * (radius))
